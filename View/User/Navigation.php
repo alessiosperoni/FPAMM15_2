da3c4617f630be320aee3a2360541e4f52985707
Alessio <alessio.speroni93@gmail.com>
2016-06-05 10:02:50 +0200

    add controller dev mod

--- View/User/Navigation.php
+++ View/User/Navigation.php
@@ -1,6 +1,7 @@
 <div id='navigation'>
     <ul>
         <li><a href="index.php?page=user&cmd=homeUser">Home</a></li>
+        <li><a href="index.php?page=user&cmd=chiSiamo">Chi siamo</a></li>
         <li><a href="index.php?page=login&cmd=logout">logout</a></li>
         <li><a href="index.php?page=user&cmd=modifica">modifica</a></li>
     </ul>
