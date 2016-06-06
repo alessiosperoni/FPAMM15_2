597a718c0543ee6cbb39b457f0b39f5b25f282db
Alessio <alessio.speroni93@gmail.com>
2016-06-06 19:22:15 +0200

    Provo a risolvere add prodotti

--- View/User/Navigation.php
+++ View/User/Navigation.php
@@ -4,5 +4,6 @@
         <li><a href="index.php?page=user&cmd=chiSiamo">Chi siamo</a></li>
         <li><a href="index.php?page=login&cmd=logout">logout</a></li>
         <li><a href="index.php?page=user&cmd=modifica">modifica</a></li>
+        <li><a href="index.php?page=user&cmd=scegliProdotto">Visualizza prodotto</a></li>
     </ul>
 </div>
