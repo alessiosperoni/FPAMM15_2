f8da9c61f9c235f88dc6e8eecb6d29c7fa57a8a6
Alessio <alessio.speroni93@gmail.com>
2016-06-07 11:22:53 +0200

    Provo a risolvere add prodotti

--- Descrizione.php
+++ Descrizione.php
@@ -74,7 +74,7 @@
         
         <p> <strong> Homepage</strong> </p>
         <ul>
-            <li> <a href="spano.sc.unica.it/amm2015/speroniAlessio/index.php?page=login">link completo alla pagina</a></li>
+            <li> <a href="http://spano.sc.unica.it/amm2015/speroniAlessio/index.php?page=login">link completo alla pagina</a></li>
             <li> <a href="index.php?page=login">Se il precedente non funziona</a></li>
         </ul>
         
